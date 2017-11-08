ed4d10893465081673c40ddbee92a6983fd6cfc9
Shaojung <shaojung@gmail.com>
2017-11-08 15:50:52 +0800

    Simple adapter 2

--- app/src/main/java/com/example/yvtc/yvtc2017110801/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvtc2017110801/MainActivity.java
@@ -2,19 +2,22 @@ package com.example.yvtc.yvtc2017110801;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView lv;
     ArrayList<Map<String, String>> mylist;
-
+    SimpleAdapter simpleAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lv = (ListView) findViewById(R.id.listView);
         mylist = new ArrayList<>();
         HashMap<String, String> m1 = new HashMap<>();
         m1.put("city", "台北");
@@ -29,5 +32,8 @@ public class MainActivity extends AppCompatActivity {
         mylist.add(m2);
         mylist.add(m3);
 
+        simpleAdapter = new SimpleAdapter(this, mylist, android.R.layout.simple_list_item_2,
+                new String[] {"city", "code"}, new int[] {android.R.id.text1, android.R.id.text2});
+        lv.setAdapter(simpleAdapter);
     }
 }
